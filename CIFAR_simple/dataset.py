6a276afb9988d04db2c01976cdf9b057f4bdd8f4
xiyori <foma@shipilov.ru>
2020-11-14 13:04:34 +0300

    Add files via upload

---
+++ CIFAR_simple/dataset.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.Tensor as Tensor
+
+
+def imshow(img: Tensor) -> None:
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# Load datasets
+batch_count = 4
+transform = transforms.ToTensor()
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_count,
+                                          shuffle=True, num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_count,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
